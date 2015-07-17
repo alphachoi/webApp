14cc3cd1ea21d174f739c9736d4f3a355687f03c
tusizi <dagger11263@gmail.com>
2015-07-17 19:44:30 +0800

    Add support for uWSGI

--- www/uwsgi.py
+++ www/uwsgi.py
@@ -6,3 +6,17 @@ if path not in sys.path:
     sys.path.append(path)
 
 from www.wsgi import Application as application
+
+if __name__ == '__main__':
+    import sqlite3
+    from www import create_db
+
+    try:
+        create_db()
+    except sqlite3.OperationalError as e:
+        print(e)
+    from wsgiref.simple_server import make_server
+
+    server = make_server('', 8080, application)
+    print('Serving HTTP on port 8080...')
+    server.serve_forever()
